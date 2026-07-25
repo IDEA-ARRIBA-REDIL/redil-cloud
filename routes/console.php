b47f1d981a251a0a317498dcbf5b67c5178ff7eb
IDEA-ARRIBA-REDIL <idea.arriba@gmail.com>
2026-07-24 22:47:03 -0500

    Revert "fix: eliminar Artisan::starting() inexistente en Laravel 11"
    This reverts commit 62b0690e8031a2dc634129e5807b18dc111aafe9.

--- routes/console.php
+++ routes/console.php
@@ -5,8 +5,15 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
+use App\Console\Commands\VerificarPagosPendientes;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Registrar la Sonda de ZonaPagos para que esté disponible en la consola Artisan
+Artisan::starting(function ($artisan) {
+    $artisan->resolve(VerificarPagosPendientes::class);
+});
 
 Schedule::command('reportes:notificar-pendientes')->everyThirtyMinutes();
